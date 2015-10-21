4f9f9d7da8f22463561c3e844bd8612610ede254
NASSER ASHRAA <3100837@ppti-14-408-06.ufr-info-p6.jussieu.fr>
2015-10-21 18:57:43 +0200

    exo 1,2

--- PR/TM5/src/multi_remonte.c
+++ PR/TM5/src/multi_remonte.c
@@ -1,106 +1,71 @@
+#define SVID_SOURCE 1
+
+#include<stdio.h>
+#include<stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/ipc.h>
-#include <sys/shm.h>
-#include <stdio.h>
-#include<stdlib.h>
+#include <sys/msg.h>
+
+#define MSG_SIZE 128
 
 int main(int argc, char* argv[]) {
-
-	int shm_id;
-	key_t cle;
-
-	int taille = 0;
-
 	int nb_fils, i, val_rand;
-	char *adr_att;
-	int *p_int;
-
-	if (argc != 2) {
-		printf("%derror syntaxe : %s nbProcessus \n", argc, argv[0]);
+	int msg_id;
+	key_t cle;
+	struct message {
+		long type;
+		int val_rand;
+	} msg;
+	if (argc < 2) {
+		printf("error syntaxe : %s nbProcessus \n", argv[0]);
 		exit(1);
 	}
-	nb_fils = atoi(argv[1]);
-
 	/*------------------------------------------------------*/
-	/* creation segment memoir	*/
+	/* creation msg	*/
 	/*------------------------------------------------------*/
-
-	taille = (nb_fils + 1) * sizeof(int);
-
-	char code = 0;
-
-	code = getpid() & 255;
-	if (code == 0) {
-		printf("error code = 0");
-		exit(-1);
-	}
-	if ((cle = ftok(argv[0], code)) == -1) {
+	char code=0;
+	code = getpid()& 255;
+	if(code == 0  ) code |= 1;
+	if((cle = ftok(argv[0], code)) ==-1){
 		perror("ftok");
 		exit(-1);
 	}
-	if ((shm_id = shmget(cle, taille, IPC_CREAT | 0666)) < 0) {
-		perror("shmget");
-		exit(-1);
-	}
-
-	if ((adr_att = shmat(shm_id, NULL, 0600)) == (void *) -1) {
-		perror("shmat");
-		exit(-1);
-	}
-
-	p_int = (int*) adr_att;
-
-	p_int[0] = 0;
+	msg_id = msgget(cle, IPC_CREAT| IPC_CREAT|0600);
 
 	/*------------------------------------------------------*/
 	/* creation processus	*/
 	/*------------------------------------------------------*/
-	int indice_fils = 1;
-	printf("pere %d nb_fils %d \n", getpid(), nb_fils);
+	nb_fils = atoi(argv[1]);
+	printf("pere %d \n", getpid());
 	for (i = 0; i < nb_fils; i++) {
 		switch (fork()) {
-		case 0:
-			/* fils*/
-
+		case 0: /* fils*/
 			srand(getpid());
 			val_rand = (int) (10 * (float) rand() / RAND_MAX);
-
-			if ((adr_att = shmat(shm_id, 0, 0600)) == (void *) -1) {
-				perror("shmat");
-				exit(-1);
-			}
-			p_int = (int*) adr_att;
-			p_int[indice_fils] = val_rand;
-			printf("fils %d, pid %d ==> %d\n", indice_fils, getpid(), val_rand);
-
-			if (indice_fils == nb_fils - 1) {
-				p_int[0] = -1;
-			}
-			shmdt(adr_att);
+			msg.type = sizeof(int);
+			msg.val_rand = val_rand;
+			msgsnd(msg_id, &msg, sizeof(int), 0);
+			printf("fils pid %d ==> %d\n", getpid(), val_rand);
 			exit(EXIT_SUCCESS);
 			break;
 		default:/* pere*/
-			indice_fils++;
+
 			break;
 		}
 	}
-	/*------------------------------------------------------*/
-	/* traitement liberation memoir partage	*/
-	/*------------------------------------------------------*/
-	while (p_int[0] == 0)
-		printf("attend while %d \n", p_int[0]);
-	sleep(1);
 
+	/*------------------------------------------------------*/
+	/* traitement liberation msg	*/
+	/*------------------------------------------------------*/
 	int result = 0;
-	for (i = 1; i < nb_fils + 1; i++) {
-
-		printf("shm%d recu %d \n", i, p_int[i]);
-		result += p_int[i];
+	for (i = 0; i < nb_fils; i++) {
+		msgrcv(msg_id, &msg, sizeof(int), sizeof(int), 0);
+		printf("msg%d recu %d \n", i, msg.val_rand);
+		result += msg.val_rand;
 	}
-	printf("pere %d : la somme est %d \n", getpid(), result);
-	shmdt(adr_att);
-	shmctl(shm_id, IPC_RMID, NULL);
+	printf("pere %d : la somme est %d \n",getpid(), result);
+	msgctl(msg_id, IPC_RMID, NULL);
 
 	return EXIT_SUCCESS;
 }
--- PR/TM5/src/remonte_ipc.c
+++ PR/TM5/src/remonte_ipc.c
@@ -9,35 +9,15 @@
 
 #define MSG_SIZE 128
 
-int main(int argc, char* argv[]) {
-	int nb_fils, i, val_rand;
-	int msg_id;
-	key_t cle;
-	struct message {
-		long type;
-		int val_rand;
-	} msg;
-	if (argc < 2) {
-		printf("error syntaxe : %s nbProcessus \n", argv[0]);
-		exit(1);
-	}
-	/*------------------------------------------------------*/
-	/* creation msg	*/
-	/*------------------------------------------------------*/
-	char code=0;
-	code = getpid()& 255;
-	if(code == 0  ) code |= 1;
-	if((cle = ftok(argv[0], code)) ==-1){
-		perror("ftok");
-		exit(-1);
-	}
-	msg_id = msgget(cle, IPC_CREAT| IPC_CREAT|0600);
+int msg_id;
+struct message {
+	long type;
+	int val_rand;
+} msg;
 
-	/*------------------------------------------------------*/
-	/* creation processus	*/
-	/*------------------------------------------------------*/
-	nb_fils = atoi(argv[1]);
+void remonte_ipc(int nb_fils) {
 	printf("pere %d \n", getpid());
+	int i, val_rand;
 	for (i = 0; i < nb_fils; i++) {
 		switch (fork()) {
 		case 0: /* fils*/
@@ -54,7 +34,52 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 	}
+}
+int main(int argc, char* argv[]) {
+	int nb_fils, i, val_rand;
+	key_t cle;
 
+	if (argc < 2) {
+		printf("error syntaxe : %s nbProcessus \n", argv[0]);
+		exit(1);
+	}
+	/*------------------------------------------------------*/
+	/* creation msg	*/
+	/*------------------------------------------------------*/
+	char code = 0;
+	code = getpid() & 255;
+	if (code == 0)
+		code |= 1;
+	if ((cle = ftok(argv[0], code)) == -1) {
+		perror("ftok");
+		exit(-1);
+	}
+	msg_id = msgget(cle, IPC_CREAT | IPC_CREAT | 0600);
+
+	/*------------------------------------------------------*/
+	/* creation processus	*/
+	/*------------------------------------------------------*/
+	nb_fils = atoi(argv[1]);
+	/*
+	 printf("pere %d \n", getpid());
+	 for (i = 0; i < nb_fils; i++) {
+	 switch (fork()) {
+	 case 0:
+	 srand(getpid());
+	 val_rand = (int) (10 * (float) rand() / RAND_MAX);
+	 msg.type = sizeof(int);
+	 msg.val_rand = val_rand;
+	 msgsnd(msg_id, &msg, sizeof(int), 0);
+	 printf("fils pid %d ==> %d\n", getpid(), val_rand);
+	 exit(EXIT_SUCCESS);
+	 break;
+	 default:
+
+	 break;
+	 }
+	 }
+	 */
+	remonte_ipc(nb_fils);
 	/*------------------------------------------------------*/
 	/* traitement liberation msg	*/
 	/*------------------------------------------------------*/
@@ -64,7 +89,7 @@ int main(int argc, char* argv[]) {
 		printf("msg%d recu %d \n", i, msg.val_rand);
 		result += msg.val_rand;
 	}
-	printf("pere %d : la somme est %d \n",getpid(), result);
+	printf("pere %d : la somme est %d \n", getpid(), result);
 	msgctl(msg_id, IPC_RMID, NULL);
 
 	return EXIT_SUCCESS;
