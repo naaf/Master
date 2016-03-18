a01450dd031ba84f72b87336a45804e23c675913
naaf <serranashraf@gmail.com>
2016-03-18 02:16:48 +0100

    add images

---
+++ semestre1/client_test/Debug/src/SDLS.d
@@ -0,0 +1,8 @@
+src/SDLS.o: ../src/SDLS.c ../src/../headers/ihm.h \
+ ../src/../headers/ricochet_robot.h ../src/../headers/SDLS.h
+
+../src/../headers/ihm.h:
+
+../src/../headers/ricochet_robot.h:
+
+../src/../headers/SDLS.h:
